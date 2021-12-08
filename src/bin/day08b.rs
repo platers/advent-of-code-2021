9004719f7a40d2bf9b2c562f53993983bfb22599
platers <platers81@gmail.com>
2021-12-08 17:21:16 -0500

    refactor day 8

--- src/bin/day08b.rs
+++ src/bin/day08b.rs
@@ -1,103 +1,40 @@
-use itertools::Itertools;
-
 fn main() {
     let input = include_str!("../../input.txt");
-    let lines = input.lines().map(|l| {
-        let (a, b) = l.split_once(" | ").unwrap();
-        let mut a = a.split_whitespace().map(|s| {
-            let mut v = s.chars().collect::<Vec<_>>();
-            v.sort(); 
-            v
-        }).collect::<Vec<_>>();
-        let b = b.split_whitespace().map(|s| {
-            let mut v = s.chars().collect::<Vec<_>>();
-            v.sort(); 
-            v
-        }).collect::<Vec<_>>();
-        a.sort();
-        (a, b)
-    }).collect::<Vec<_>>();
-    let alph = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g'];
-    let base = vec![
-        "abcefg",
-        "cf",
-        "acdeg",
-        "acdfg",
-        "bcdf",
-        "abdfg",
-        "abdefg",
-        "acf",
-        "abcdefg",
-        "abcdfg"
-    ].iter().map(|s| s.chars().collect::<Vec<_>>()).collect::<Vec<_>>();
-    let mut base_sorted = base.clone();
-    base_sorted.sort();
-
-
-    let ans = lines.iter().map(|(a, b)| {
-        let mut perm = vec![];
-        for p in alph.iter().permutations(alph.len()) {
-            // println!("{:?}", apply_permutation(&p, a, &alph));
-            if apply_permutation(&p, a, &alph, true) == base_sorted {
-                println!("{:?}", p);
-                perm = p; 
-                break;
-            }
-        };
-
-        let n = apply_permutation(&perm, b, &alph, false).iter()
-            .map(|w| {
-                base.iter().position(|b| b == w).unwrap()
+    let ans = input
+        .lines()
+        .map(|line| {
+            let (inputs, outputs) = line.split_once(" | ").unwrap();
+            let inputs = inputs.split_whitespace().collect::<Vec<_>>();
+            let outputs = outputs.split_whitespace().collect::<Vec<_>>();
+            let one = inputs.iter().find(|w| w.len() == 2).unwrap();
+            let four = inputs.iter().find(|w| w.len() == 4).unwrap();
+            
+            outputs.iter().map(|word| {
+                match word.len() {
+                    2 => 1,
+                    4 => 4,
+                    3 => 7,
+                    7 => 8,
+                    _ => {
+                        let one = intersection_len(one, &word);
+                        let four = intersection_len(four, &word);
+                        match (word.len(), one, four) {
+                            (5, 1, 2) => 2,
+                            (5, 1, 3) => 5,
+                            (6, 1, 3) => 6,
+                            (6, 2, 4) => 9,
+                            (6, 2, 3) => 0,
+                            (5, 2, 3) => 3,
+                            _ => panic!("{} {} {}", word.len(), one, four)
+                        }
+                    }
+                }
             })
-            .map(|i| char::from_digit(i as u32, 10).unwrap())
-            .collect::<String>()
-            .parse::<usize>().unwrap();
-        println!("{}", n);
-        n
-    }).sum::<usize>();
-
+            .fold(0, |acc, d| acc * 10 + d)
+        }).sum::<u32>();
     println!("{}", ans);
 }
 
-fn apply_permutation(perm: &Vec<&char>, base: &Vec<Vec<char>>, alph: &Vec<char>, sort: bool) -> Vec<Vec<char>> {
-    // println!("{:?}", perm);
-    let mut q = base.iter().map(|b| {
-        let mut v = b.iter().map(|c| {
-            *perm[alph.iter().position(|a| a == c).unwrap()]
-        }).collect::<Vec<_>>();
-        v.sort();
-        v
-    }).collect::<Vec<_>>();
-    if sort {
-        q.sort();
-    }
-    q
-}
-
-// tests
-#[test]
-fn test_apply_permutation() {
-    let alph = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g'];
-    let perm = alph
-        .iter()
-        .permutations(7)
-        .nth(0) 
-        .unwrap();
-    println!("{:?}", perm);
-    let base = vec![
-        "abcef",
-        "cf",
-        "acdeg",
-        "acdfg",
-        "bcdf",
-        "abdfg",
-        "abdefg",
-        "abcdefg",
-        "abcdfg"
-    ].iter().map(|s| s.chars().collect::<Vec<_>>()).collect::<Vec<_>>();
-
-    let mut base_sorted = base.clone();
-    base_sorted.sort();
-
-    assert_eq!(apply_permutation(&perm, &base_sorted, &alph, true), base_sorted);
+fn intersection_len(a: &str, b: &str) -> usize {
+    a.chars().map(|c| b.contains(c)).filter(|x| *x).count()
 }
