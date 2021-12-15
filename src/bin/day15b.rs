ccb76377e03f396619ac168fbf74ac5fa1196979
platers <platers81@gmail.com>
2021-12-15 03:08:33 -0500

    day15

---
+++ src/bin/day15b.rs
@@ -0,0 +1,45 @@
+use std::collections::HashMap;
+use std::collections::BinaryHeap;
+use std::cmp::Reverse;
+
+fn main() {
+    let input = include_str!("../../input.txt");
+    let mut risk = HashMap::new();
+    let n = input.lines().count() as i32;
+    let m = input.lines().next().unwrap().len() as i32;
+
+    for (i, line) in input.lines().enumerate() {
+        for (j, c) in line.chars().enumerate() {
+            risk.entry((i as i32, j as i32)).or_insert(c.to_digit(10).unwrap() as i32);
+        }
+    }
+
+    let mut dist = HashMap::new();
+    let mut queue = BinaryHeap::new();
+    queue.push(Reverse(((0, 0), 0)));
+    dist.insert((0i32, 0i32), 0);
+
+    while let Some(Reverse(((x, y), d))) = queue.pop() {
+        // println!("{:?}", (x, y));
+        for (dx, dy) in &[(0, -1), (0, 1), (-1, 0), (1, 0)] {
+            let (nx, ny) = (x + dx, y + dy);
+            if nx < 0 || ny < 0 || nx >= n * 5 || ny >= m * 5 {
+                continue;
+            }
+
+            let s = risk.get(&(nx % n, ny % m)).unwrap();
+            let r = nx / n + ny / m;
+            let nd = d + if s + r > 9 {
+                (s + r + 1) % 10
+            } else {
+                s + r
+            };
+            if !dist.contains_key(&(nx, ny)) || dist.get(&(nx, ny)).unwrap() > &nd {
+                dist.insert((nx, ny), nd);
+                queue.push(Reverse(((nx, ny), nd)));
+            }
+        }
+    }
+
+    println!("{}", dist.get(&(n * 5 - 1, m * 5 - 1)).unwrap());
+}
